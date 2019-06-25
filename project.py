e7368948d46802cbebf972dd238698949d32f6c4
unknown <eva.lapp24@gmail.com>
2019-06-25 16:12:35 -0600

    add data

--- project.py
+++ project.py
@@ -11,49 +11,107 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 # Migrate(app,db)
 
-class Customer(db.Model):
-	__tablename__ = 'customer'
-	id = db.Column(db.Integer, primary_key = True)
-	customer_name = db.Column(db.Text)
-	contact = db.Column(db.Text)
-	phone = db.Column(db.Text)
+class Student(db.Model):
+	__tablename__ = 'student'
+	student_id = db.Column(db.Integer, primary_key = True)
+	first_name = db.Column(db.Text)
+	last_name = db.Column(db.Text)
+	linkedIn = db.Column(db.Text)
+	github = db.Column(db.Text)
+	comp_id = db.Column(db.Integer)
+	project_id = db.Column(db.Integer)
+	 # db.ForeignKey('competency.id')
 
-	def __init__(self, customer_name, contact, phone):
-		self.customer_name = customer_name
-		self.contact = contact
-		self.phone = phone	
-
-	def __repr__(self):
-		return f"Customer: {self.customer_name} Contact:{self.contact} Phone:{self.phone}"
-
-class Invoice(db.Model):
-	__tablename__ = 'invoice'
-	id = db.Column(db.Integer, primary_key = True)
-	customer_id = db.Column(db.Integer)
-	# , db.ForeignKey ('customer.id')
-	date = db.Column(db.Date)
-
-	def __init__(self, customer_id, date):
-		self.customer_id = customer_id
-		self.date = date
+	def __init__(self, first_name, last_name, linkedIn, github, comp_id, project_id):
+		self.first_name = first_name
+		self.last_name = last_name
+		self.linkedIn = linkedIn
+		self.github = github
+		self.comp_id = comp_id
+		self.project_id = project_id
 
 	def __repr__(self):
-		return f" This is a test ,{self.customer_id}, {self.date}"
+		return f"Student:{self.first_name}{self.last_name} Student ID: {self.student_id}"
 
-@app.route("/info/")
+class Competency(db.Model):
+	__tablename__ = 'competency'
+	comp_id = db.Column(db.Integer, primary_key = True)
+	comp_name = db.Column(db.Text)
+	comp_num = db.Column(db.Integer)
+	unit_effort = db.Column(db.Integer)
+	percent_complete = db.Column(db.Float)
+
+	def __init__(self, comp_name, comp_num,unit_effort, percent_complete):	
+		self.comp_name = comp_name
+		self.comp_num = comp_num
+		self.unit_effort = unit_effort
+		self.percent_complete = percent_complete
+
+	def __repr__(self):
+		return f" This is a test ,{self.comp_name}, {self.comp_id}"
+
+class Project(db.Model):
+	__tablename__ = 'project'
+	project_id = db.Column(db.Integer, primary_key = True)
+	project_name = db.Column(db.Text)
+
+	def __init__(self, project_name):
+		self.project_name = project_name
+
+	def __repr__(self):
+		return f"{self.project_name}"
+
+
+
+@app.route("/student/")
 def build():
-	customer_list_master = []
-	customer_list = Customer.query.all()
-	print(customer_list)
-	for customer in customer_list:
-		customer_list_dict = {
-		'Name': customer.customer_name,
-		'Contact' : customer.contact,
-		'Phone' : customer.phone,
+	student_list_master = []
+	student_list = Student.query.all()
+	print(student_list)
+	for student in student_list:
+		student_list_dict = {
+		'Student ID': student.student_id,
+		'First Name': student.first_name,
+		'Last Name' : student.last_name,
+		'LinkedIn' : student.linkedIn,
+		'GitHub' : student.github,
+		'Competency' : student.comp_id,
+		'Project': student.project_id
 		}
-		print(customer.customer_name)
-		customer_list_master.append(customer_list_dict)
-	return json.dumps(customer_list_master)
+		print(student.first_name, student.last_name)
+		student_list_master.append(student_list_dict)
+	return json.dumps(student_list_master)
+
+
+@app.route("/comp/")
+def comp():
+	comp_list_master = []
+	comp_list = Competency.query.all()
+	print(comp_list)
+	for comp in comp_list:
+		comp_list_dict = {
+		'Competency ID': comp.comp_id	,
+		'Competency Number' : comp.comp_num,
+		'Competency Name' : comp.comp_name,
+		'Units Effort': comp.unit_effort,
+		'Percent Complete': comp.percent_complete,
+		}
+		print(comp.comp_num, comp.comp_name)
+		comp_list_master.append(comp_list_dict)
+	return json.dumps(comp_list_master)
+
+@app.route("/project/")
+def project():
+	project_list_master = []
+	project_list = Project.query.all()
+	for project in project_list:
+		project_list_dict = {
+		'Project ID' : project.project_id,
+		'Project Name' : project.project_name
+		}
+		print (project.project_name)
+		project_list_master.append(project_list_dict)
+	return json.dumps(project_list_master)	
 
 
 if __name__ == '__main__':
--- projectdata.py
+++ projectdata.py
@@ -1,25 +1,50 @@
-from project import db, Customer, Invoice
-import datetime
+from project import db, Student, Competency, Project
 
 db.create_all()
 
-Streamside = Customer('Streamside Welding and Fabrication', 'Jeff Lapp', 4036199773)
-Grizzly = Customer('Grizzly Welding Services', 'Chris Sebolsky', 4039901234)
-Freestone = Customer('Freestone Welding Ltd', 'Daniel Lachance', 4036671298 )
+Eva = Student('Eva', 'Lapp', 'https://www.linkedin.com/in/eva-lapp-01b27a60/', 'https://github.com/EvaLappski', 250, 1)
+Dima = Student('Dima', 'Marachi', 'www.linkedin.com/in/dima-marachi', 'https://github.com/dmarachi/', 250, 2)
+Shane = Student('Shane', 'McGuire', 'www.linkedin.com/in/shane-mcguire-100 ', 'https://github.com/shanemcg88/evolveu', 930, 1)
+a = Competency('Python - Full Stack',250, 4, 93.94)
+b = Competency('PostgreSQL', 930, 3, 87.88)
+c = Competency('Python - Flask', 240, 3, 83.33)
+d = Competency('Javascript Basic Logic', 100, 5, 7.58)
+e = Competency("Javascript Events/DOM", 110, 8, 19.70)
+f = Competency('Git', 910, 2, 22.73)
+g = Competency('ReactJS', 120, 3, 27.27)
+h = Competency('Javascript TDD', 130, 2, 30.30)
+i = Competency('Javascript OO', 140, 14, 51.52)
+j = Competency('Javascript Algorithms', 150, 4, 57.58)
+k = Competency('Javascript Open API JSON', 160, 1, 59.09)
+l = Competency('Javascript React Redux ', 170, 3, 63.64)
+m = Competency('D3', 180, 2, 66.67)
+n = Competency('Front End Checklist', 198, 2, 69.70)
+o = Competency('Python - Getting Started - Logic', 200, 2, 72.73)
+p = Competency('Python - Environment Conda', 210, 1, 74.24)
+q = Competency('Python - File IO', 220, 1, 75.76)
+r = Competency('Python - Excel', 230, 2, 78.79)
+s = Competency('Heroku', 940, 2, 96.97)
+t = Competency("Docker", 950, 1, 99.98)
+u = Competency("Back End Checklist", 298, 1, 100.00)
+mindfuel = Project('MindFuel')
+cultivatr = Project('Cultivatr')
+library = Project('Calgary Public Library')
+rainforester = Project('Rainforester')
 
-print(Grizzly.id)
+print(Eva.student_id)
 
-db.session.add_all([Streamside,Grizzly,Freestone])
+db.session.add_all([Eva, Dima, Shane])
+db.session.add_all([ a,b,c,d,e,f,g,h,i,j,k,l,m])
+db.session.add_all([mindfuel, cultivatr, library, rainforester])
 
 db.session.commit()
 
-print(Grizzly.id)
+print(Eva.student_id)
 
-db.create_all()
-one = Invoice(1,datetime.datetime(2006-01-01))
-two = Invoice(2,datetime.datetime(2009-06-01))
 
-db.session.add_all([one, two])
-db.session.commit()
+print(c.comp_num)
+print(mindfuel.project_name)
 
-print("test",one.date)
+
+
+
